e12e21aa17e1bf2d2c07e7da97864b9d7be7a354
Pedro <castro.fcefyn@gmail.com>
2020-03-13 00:03:39 -0300

    Modelos en Angular. Agredo zapatilla.ts y la uso en zapatilla.component.ts

---
+++ aprendo-angular/src/app/models/zapatilla.ts
@@ -0,0 +1,31 @@
+export class Zapatilla{
+/*
+    public nombre: string;
+    public marca: string;
+    public color: string;
+    public precio: number;
+    public stock: boolean;
+
+    constructor(nombre, marca, color, precio, stock){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.color = color;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    */
+
+    //Lo anterior es equivalente a:
+
+    constructor(
+        public nombre: string,
+        public marca: string,
+        public color: string,
+        public precio: number,
+        public stock: boolean
+    ){
+        
+    }
+}
+
+
