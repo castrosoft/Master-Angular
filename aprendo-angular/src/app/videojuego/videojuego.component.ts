c9a0b01c236af27d3d487b4c835c1a4af7e3889f
Pedro <castro.fcefyn@gmail.com>
2020-03-12 21:51:32 -0300

    Cargo el componente videojuego y lo muestro en la app

--- aprendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-videojuego',
+  selector: 'videojuego',
   templateUrl: './videojuego.component.html',
   styleUrls: ['./videojuego.component.css']
 })
